c326c6eacba59894e33327f870f85f1778cc5aa3
Gray Nance <graynance@gmail.com>
2022-12-14 11:14:01 -0800

    updateProduct form persistence / optional chaining for createProduct

--- react-app/src/components/ProductForm/ProductForm.js
+++ react-app/src/components/ProductForm/ProductForm.js
@@ -1,6 +1,8 @@
+import { useState, useEffect } from 'react';
 import './ProductForm.css';
 
 const ProductForm = ({
+  product,
   formType,
   handleSubmit,
   title,
@@ -18,120 +20,143 @@ const ProductForm = ({
   errors,
   setErrors
 }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+    setTitle(product?.title);
+    setDescription(product?.description);
+    set_detailed_description(product?.detailedDescription);
+    set_category_id(product?.categoryId);
+    setPrice(product?.price);
+  }, [
+    product?.title,
+    product?.description,
+    product?.detailedDescription,
+    product?.categoryId,
+    product?.price,
+    setTitle,
+    setDescription,
+    set_detailed_description,
+    set_category_id,
+    setPrice
+  ]);
   return (
     <>
-      <div className='product-form-container'>
-        <form
-          className='product-form'
-          onSubmit={handleSubmit}
-        >
-          {errors.length > 0 ? (
-            <ul className='product-form-header'>
-              {errors.map((error, idx) => (
-                <li key={idx}>{error}</li>
-              ))}
-            </ul>
-          ) : formType === 'create' ? (
-            <h2 className='product-form-header'>Provide some details about your product</h2>
-          ) : (
-            <h2 className='product-form-header'>Change any product details</h2>
-          )}
+      {isLoaded && (
+        <div className='product-form-container'>
+          <form
+            className='product-form'
+            onSubmit={handleSubmit}
+          >
+            {errors.length > 0 ? (
+              <ul className='product-form-header'>
+                {errors.map((error, idx) => (
+                  <li key={idx}>{error}</li>
+                ))}
+              </ul>
+            ) : formType === 'create' ? (
+              <h2 className='product-form-header'>Provide some details about your product</h2>
+            ) : (
+              <h2 className='product-form-header'>Change any product details</h2>
+            )}
 
-          <div className='inputs-container'>
-            <div className='title-container'>
-              <label htmlFor='title-input'>TITLE</label>
-              <input
-                className='title-input'
-                onChange={e => setTitle(e.target.value)}
-                name='title-input'
-                placeholder='Enter a title'
-                required
-                type='text'
-                value={title}
-              />
-            </div>
-            <div className='description-container'>
-              <label htmlFor='desctiption-input'>DESCRIPTION</label>
-              <input
-                className='description-input'
-                onChange={e => setDescription(e.target.value)}
-                name='description-input'
-                placeholder='Enter a brief description'
-                required
-                type='text'
-                value={description}
-              />
-            </div>
-            <div className='detailed-description-container'>
-              <label htmlFor='detailed-description-input'>DETAILS</label>
-              <textarea
-                className='detailed-description-input'
-                onChange={e => set_detailed_description(e.target.value)}
-                name='detailed-description-input'
-                placeholder='Enter all the details'
-                value={detailed_description}
-              />
-            </div>
-            <div className='category-container'>
-              <label htmlFor='category-input'>CATEGORY</label>
-              <select
-                className='category-input'
-                onChange={e => set_category_id(e.target.value)}
-                name='category-input'
-                required
-                value={category_id}
-              >
-                <option
-                  value={category_id === 0 ? category_id : ''}
-                  disabled
-                  hidden
-                >
-                  Select a Category
-                </option>
-                <option value='1'>Gear</option>
-                <option value='2'>Style</option>
-                <option value='3'>Cars</option>
-                <option value='4'>Tech</option>
-                <option value='5'>Shelter</option>
-                <option value='6'>Vices</option>
-                <option value='7'>Body</option>
-                <option value='8'>Etc</option>
-              </select>
-            </div>
-            <div className='price-container'>
-              <label htmlFor='price-input'>PRICE</label>
-              <input
-                className='price-input'
-                min={0}
-                onChange={e => setPrice(e.target.value)}
-                placeholder='Enter a price'
-                required
-                type='number'
-                value={price}
-              />
-            </div>
-            {formType === 'create' && (
-              <div className='preview-img-container'>
-                <label htmlFor='preview-img-input'>MAIN IMAGE</label>
+            <div className='inputs-container'>
+              <div className='title-container'>
+                <label htmlFor='title-input'>TITLE</label>
                 <input
-                  className='preview-img-input'
-                  onChange={e => set_preview_img_url(e.target.value)}
-                  placeholder='Enter a URL'
+                  className='title-input'
+                  onChange={e => setTitle(e.target.value)}
+                  name='title-input'
+                  placeholder='Enter a title'
                   required
-                  type='url'
-                  value={preview_img_url}
+                  type='text'
+                  value={title}
                 />
               </div>
-            )}
-            <button
-              className='product-form-submit'
-              type='submit'
-            >
-              SUBMIT
-            </button>
-          </div>
-        </form>
-      </div>
+              <div className='description-container'>
+                <label htmlFor='desctiption-input'>DESCRIPTION</label>
+                <input
+                  className='description-input'
+                  onChange={e => setDescription(e.target.value)}
+                  name='description-input'
+                  placeholder='Enter a brief description'
+                  required
+                  type='text'
+                  value={description}
+                />
+              </div>
+              <div className='detailed-description-container'>
+                <label htmlFor='detailed-description-input'>DETAILS</label>
+                <textarea
+                  className='detailed-description-input'
+                  onChange={e => set_detailed_description(e.target.value)}
+                  name='detailed-description-input'
+                  placeholder='Enter all the details'
+                  value={detailed_description}
+                />
+              </div>
+              <div className='category-container'>
+                <label htmlFor='category-input'>CATEGORY</label>
+                <select
+                  className='category-input'
+                  onChange={e => set_category_id(e.target.value)}
+                  name='category-input'
+                  required
+                  value={category_id}
+                >
+                  <option
+                    value={category_id === 0 ? category_id : ''}
+                    disabled
+                    hidden
+                  >
+                    Select a Category
+                  </option>
+                  <option value='1'>Gear</option>
+                  <option value='2'>Style</option>
+                  <option value='3'>Cars</option>
+                  <option value='4'>Tech</option>
+                  <option value='5'>Shelter</option>
+                  <option value='6'>Vices</option>
+                  <option value='7'>Body</option>
+                  <option value='8'>Etc</option>
+                </select>
+              </div>
+              <div className='price-container'>
+                <label htmlFor='price-input'>PRICE</label>
+                <input
+                  className='price-input'
+                  min={0}
+                  onChange={e => setPrice(e.target.value)}
+                  placeholder='Enter a price'
+                  required
+                  type='number'
+                  value={price}
+                />
+              </div>
+              {formType === 'create' && (
+                <div className='preview-img-container'>
+                  <label htmlFor='preview-img-input'>MAIN IMAGE</label>
+                  <input
+                    className='preview-img-input'
+                    onChange={e => set_preview_img_url(e.target.value)}
+                    placeholder='Enter a URL'
+                    required
+                    type='url'
+                    value={preview_img_url}
+                  />
+                </div>
+              )}
+              <button
+                className='product-form-submit'
+                type='submit'
+              >
+                SUBMIT
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </>
   );
 };
